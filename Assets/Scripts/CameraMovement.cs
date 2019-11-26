cc1bc812002ad94fb23122a1f872169f1e183cf4
Pierce Lauring <pclauring@gmail.com>
2019-11-25 21:44:18 -0500

    Astronaut First Commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform Target;
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame Last
+    void LateUpdate()
+    {
+        if(transform.position != Target.position)
+        {
+            Vector3 targetPosition = new Vector3(Target.position.x, Target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Speed);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D Player;
+    private Vector3 Change;
+    private Animator Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Change = Vector3.zero;
+        Change.x = Input.GetAxisRaw("Horizontal");
+        Change.y = Input.GetAxisRaw("Vertical");
+        if(Change != Vector3.zero)
+        {
+            MovePlayer();
+            Animator.SetFloat("MoveX", Change.x);
+            Animator.SetFloat("MoveY", Change.y);
+            Animator.SetBool("Moving", true);
+        } else
+        {
+            Animator.SetBool("Moving", false);
+        }
+    }
+
+    void MovePlayer()
+    {
+        Player.MovePosition(
+            transform.position + Change * Speed * Time.deltaTime
+            );
+    }
+}
