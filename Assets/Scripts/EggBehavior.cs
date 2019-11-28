511a8fde77d646cff1ecfa208900af01eb2d7e37
Pierce Lauring <pclauring@gmail.com>
2019-11-27 19:54:04 -0500

    Hitboxes Attack animations

---
+++ Assets/Scripts/EggBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggBehavior : MonoBehaviour
+{
+    private Animator Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Burst()
+    {
+        Animator.SetBool("Hit", true);
+        StartCoroutine(BurstCoroutine());
+    }
+
+    IEnumerator BurstCoroutine()
+    {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+}
+
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerState PlayerState;
     public float Speed;
     private Rigidbody2D Player;
     private Vector3 Change;
     private Animator Animator;
+
     // Start is called before the first frame update
     void Start()
     {
+        PlayerState = PlayerState.walk;
         Player = GetComponent<Rigidbody2D>();
         Animator = GetComponent<Animator>();
     }
@@ -21,16 +31,38 @@ public class PlayerMovement : MonoBehaviour
         Change = Vector3.zero;
         Change.x = Input.GetAxisRaw("Horizontal");
         Change.y = Input.GetAxisRaw("Vertical");
-        if(Change != Vector3.zero)
+        if (Input.GetButtonDown("Attack") && PlayerState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCoroutine());
+        } else if (PlayerState == PlayerState.walk)
+        {
+            MoveAndAnimatePlayer();
+        }
+        
+    }
+    private void MoveAndAnimatePlayer()
+    {
+        if (Change != Vector3.zero)
         {
             MovePlayer();
             Animator.SetFloat("MoveX", Change.x);
             Animator.SetFloat("MoveY", Change.y);
             Animator.SetBool("Moving", true);
-        } else
+        }
+        else
         {
             Animator.SetBool("Moving", false);
         }
+    }
+
+    private IEnumerator AttackCoroutine()
+    {
+        Animator.SetBool("Attacking", true);
+        PlayerState = PlayerState.attack;
+        yield return null;
+        Animator.SetBool("Attacking", false);
+        yield return new WaitForSeconds(.1f);
+        PlayerState = PlayerState.walk;
     }
 
     void MovePlayer()
