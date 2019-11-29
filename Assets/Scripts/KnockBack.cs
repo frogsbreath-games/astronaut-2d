1755662981a98ae066472fa9f83eeaa15ef8c170
Pierce Lauring <pclauring@gmail.com>
2019-11-29 16:42:25 -0500

    Knockback Enemy AI 1.0

---
+++ Assets/Scripts/KnockBack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+{
+    public float Thrust;
+    public float KnockbackTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody2D enemy = collision.GetComponent<Rigidbody2D>();
+            if (enemy != null)
+            {
+                enemy.isKinematic = false;
+                Vector2 difference = enemy.transform.position - transform.position;
+                difference = difference.normalized * Thrust;
+                enemy.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockbackTimeout(enemy));
+            }
+        }
+    }
+
+    IEnumerator KnockbackTimeout(Rigidbody2D rigidbody)
+    {
+        if (rigidbody != null)
+        {
+            yield return new WaitForSeconds(KnockbackTime);
+            rigidbody.velocity = Vector2.zero;
+            rigidbody.isKinematic = true;
+        }
+    }
+}
---
+++ Assets/Scripts/OozeBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OozeBehavior : EnemyBehavior
+{
+    public Transform Target;
+    public float ChaseRadius;
+    public float AttackRadius;
+    public Transform HomePosition;
+    private Animator Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = GameObject.FindWithTag("Player").transform;
+        Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(Target.position
+            , transform.position) <= ChaseRadius 
+            && Vector3.Distance(Target.position
+            , transform.position) > AttackRadius)
+        {
+            Animator.SetBool("Alert", true);
+            transform.position = Vector3.MoveTowards(transform.position, Target.position, MovementSpeed * Time.deltaTime);
+        } else
+        {
+            Animator.SetBool("Alert", false);
+        }
+    }
+}
