e59aa599f1442ec6b42a75674ee5501a754517f2
Pierce Lauring <pclauring@gmail.com>
2019-12-04 21:48:02 -0500

    Signal & Listener Added

---
+++ Assets/Scripts/ScriptableObjects/SignalEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SignalEvent : ScriptableObject
+{
+    List<SignalEventListener> Listeners = new List<SignalEventListener>();
+
+    public void Raise()
+    {
+        for (int i = Listeners.Count - 1; i >= 0; i--)
+        {
+            Listeners[i].OnSignalEventRaise();
+        }
+    }
+    
+    public void RegisterListener(SignalEventListener listener)
+    {
+        Listeners.Add(listener);   
+    }
+
+    public void RemoveListener(SignalEventListener listener)
+    {
+        Listeners.Remove(listener);
+    }
+}
---
+++ Assets/Scripts/SignalEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SignalEventListener : MonoBehaviour
+{
+    public SignalEvent Signal;
+    public UnityEvent UnityEvent;
+
+    public void OnSignalEventRaise()
+    {
+        UnityEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        Signal.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Signal.RegisterListener(this);
+    }
+}
